899e960bea6078ab4434a383b78ab6b167528c99
Saman Sattari <samansattarimaleki@teknasyon.com>
2024-07-07 16:42:32 +0300

    optimize imports

--- app/src/main/java/com/turkurt656/jetpacklayouts/app/ui/screen/MainScreen.kt
+++ app/src/main/java/com/turkurt656/jetpacklayouts/app/ui/screen/MainScreen.kt
@@ -2,16 +2,11 @@ package com.turkurt656.jetpacklayouts.app.ui.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.turkurt656.jetpacklayouts.app.NavigateButton
 
 @Composable
--- app/src/main/java/com/turkurt656/jetpacklayouts/app/ui/screen/circular/RadiusAngleCircular.kt
+++ app/src/main/java/com/turkurt656/jetpacklayouts/app/ui/screen/circular/RadiusAngleCircular.kt
@@ -7,9 +7,12 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
